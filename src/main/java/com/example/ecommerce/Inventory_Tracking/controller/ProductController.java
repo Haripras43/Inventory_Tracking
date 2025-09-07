fd1036067b87c6ca9d8f2b07c8218963d5cd936a
Hari prassanna <jrhariprassanna@gmail.com>
2025-09-07 20:11:52 +0530

    Adding changes

--- src/main/java/com/example/ecommerce/Inventory_Tracking/controller/ProductController.java
+++ src/main/java/com/example/ecommerce/Inventory_Tracking/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.example.ecommerce.Inventory_Tracking.dto.ProductDto;
 import com.example.ecommerce.Inventory_Tracking.entity.Product;
 import com.example.ecommerce.Inventory_Tracking.service.ProductImpl;
 import com.example.ecommerce.Inventory_Tracking.service.ProductService;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,9 +30,20 @@ public class ProductController {
         List<ProductDto> products = productService.getAllProducts();
         return ResponseEntity.ok(products);
 }
-@GetMapping
+@GetMapping("/{id}")
     public ResponseEntity<ProductDto> getProductByStyleId(@PathVariable Long styleId){
         ProductDto getProductByStyleID = productService.getProductById(styleId);
         return ResponseEntity.ok(getProductByStyleID);
 }
+@PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(@PathVariable("id") Long styleId, @RequestBody ProductDto productDto){
+        ProductDto updatedProduct = productService.updateProduct(styleId,productDto);
+        return new ResponseEntity<>(updatedProduct,HttpStatus.OK);
+}
+@DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteByStyleId(@PathVariable("id") Long styleId){
+        productService.deleteProduct(styleId);
+        return ResponseEntity.ok("Delete successfully");
+}
+
 }
